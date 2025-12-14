ffe2df59f2df6045ae2f895b653a2e5db6ed8180
王鼎淇 <2070651521@qq.com>
2025-12-14 17:31:20 +0800

    初始化新项目

---
+++ ooad-2-8/src/test/java/cn/edu/xmu/oomall/ooad/Ooad28ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.oomall.ooad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ooad28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
